97d8465cdab220b416bbdeefe5e50b19931b44aa
YogiPaturu <ypaturu@gmail.com>
2021-11-11 17:15:26 -0600

    pre-merge

--- server/controllers/CookieController.js
+++ server/controllers/CookieController.js
@@ -4,8 +4,8 @@ CookieController.setSSIDCookie = (req, res, next) => {
   res.cookie('ssid', res.locals.id, { httpOnly: true })
   console.log('cookie is set')
   next();
-
 }
+
 CookieController.clearCookie = (req, res, next) => {
 
   res.clearCookie('ssid');
